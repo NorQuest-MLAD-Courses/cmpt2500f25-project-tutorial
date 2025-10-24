12fce753c31c847e5eff421ca8236aff28cdf05d
Mohammad M. Ajallooeian <ajallooe@users.noreply.github.com>
2025-10-24 01:33:10 +0000

    train.py updated.

--- src/train.py
+++ src/train.py
@@ -1,7 +1,25 @@
+import argparse
+import joblib
+import pandas as pd
 from sklearn.ensemble import AdaBoostClassifier
 
-def train(X, y):
-    clf = AdaBoostClassifier()
+def train_model(X, y, n_estimators):
+    clf = AdaBoostClassifier(n_estimators=n_estimators)
     clf.fit(X, y)
     return clf
 
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Train churn prediction model.")
+    parser.add_argument("--input_data_path", type=str, default="data/processed/processed_1.csv")
+    parser.add_argument("--output_model_path", type=str, default="models/result.pkl")
+
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.input_data_path)
+    X = df.drop('label').values
+    y = df['label'].values
+    clf = train_model(X, y, 25)
+
+    # Save the model
+    joblib.dump(clf, args.output_model_path)
